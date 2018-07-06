785a8ab7f627da94d5cab1faf4d4ec5f437de14f
krisztin <k.nagy@wellcome.ac.uk>
2018-07-06 17:21:59 +0100

    adding functions to change posts post type to news in the back and front ends

--- functions.php
+++ functions.php
@@ -196,10 +196,40 @@ function charity_shelter_cat_init() {
 		'query_var'          => true,
 		'rewrite'            => array( 'slug' => 'cat' ),
 		'capability_type'    => 'post',
-		'has_archive'        => true,
-		'hierarchical'       => false,
-		'menu_position'      => null,
+		'menu_position'      => 20,
 		'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments', 'custom-fields' )	);
 
 	register_post_type( 'cats', $args );
 }
+
+// changing Post post type to news in the backend
+
+function revcon_change_post_label() {
+	global $menu;
+	global $submenu;
+	$menu[5][0] = 'News';
+	$submenu['edit.php'][5][0] = 'News';
+	$submenu['edit.php'][10][0] = 'Add News';
+	$submenu['edit.php'][16][0] = 'News Tags';
+}
+function revcon_change_post_object() {
+	global $wp_post_types;
+	$labels = &$wp_post_types['post']->labels;
+	$labels->name = 'News';
+	$labels->singular_name = 'News';
+	$labels->add_new = 'Add News';
+	$labels->add_new_item = 'Add News';
+	$labels->edit_item = 'Edit News';
+	$labels->new_item = 'News';
+	$labels->view_item = 'View News';
+	$labels->search_items = 'Search News';
+	$labels->not_found = 'No News found';
+	$labels->not_found_in_trash = 'No News found in Trash';
+	$labels->all_items = 'All News';
+	$labels->menu_name = 'News';
+	$labels->name_admin_bar = 'News';
+	$args->rewrite = array('slug' => 'news');
+}
+
+add_action( 'admin_menu', 'revcon_change_post_label' );
+add_action( 'init', 'revcon_change_post_object' );
