c238a3734266210725f4599fa9e591765f44b877
krisztin <k.nagy@wellcome.ac.uk>
2018-07-09 14:14:02 +0100

    adding featured cat to 404 page

--- 404.php
+++ 404.php
@@ -11,45 +11,39 @@ get_header();
 ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+		<main id="main" class="site-main container-full">
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'charity_shelter' ); ?></h1>
+					<h1 class="page-title"><?php esc_html_e( 'Oops! Page not found', 'charity_shelter' );?></h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'charity_shelter' ); ?></p>
-
+					<p><?php esc_html_e( 'It looks like nothing was found at this location.', 'charity_shelter' ); ?></p>
+					<h2 class="pop">Maybe you were looking for me?</h2>
+				
 					<?php
-					get_search_form();
+					$args = array(
+						'post_type' => 'cats',
+						'meta_key' => 'featured',
+						'meta_value' => '1'
+					);
 
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
+					$query = new WP_Query($args);
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'charity_shelter' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$charity_shelter_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'charity_shelter' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$charity_shelter_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
+					while ( $query->have_posts() ) : $query->the_post(); ?>
+						<section>
+							<a href="<?php the_permalink(); ?>">
+								<h1><?php the_title(); ?></h1>
+								<p><?php the_field( 'featured_excerpt'); ?></p>
+								<?php the_post_thumbnail(); ?>
+							</a>
+						</section>
+				<?php endwhile;
+				wp_reset_query();
+				?>					
+				<p>Or search the site for what you're looking for.</p><?php
+					get_search_form(); ?>
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
