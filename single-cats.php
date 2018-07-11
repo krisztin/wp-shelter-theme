78a6467c32f3d0de3b1e6d2c17f3e924771894a7
krisztin <k.nagy@wellcome.ac.uk>
2018-07-11 14:38:40 +0100

    reworking single cat header

--- single-cats.php
+++ single-cats.php
@@ -5,9 +5,14 @@
 
 		<?php
 		while ( have_posts() ) :
-			the_post(); 
-
-			get_template_part( 'template-parts/content', 'page' ); ?>
+			the_post(); ?>
+			<header class="entry-header container-full">
+				<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+			</header>
+	<!-- .entry-header -->
+			<section class="container-wide">
+			<?php the_post_thumbnail(); ?>
+			</section>
 			<article class="cat-info container-full">
 				<h3>Age: <?php the_field('cat_age'); ?></h3>
 				<h3>Can live with dogs? <?php the_field('likes_dogs'); ?></h3>
