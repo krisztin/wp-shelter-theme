58199c8b37778fe1c49510b31cb0060d9faea5e4
krisztin <k.nagy@wellcome.ac.uk>
2018-07-11 21:35:34 +0100

    adding some structure and classes to featured cat

--- template-parts/content-featured.php
+++ template-parts/content-featured.php
@@ -8,12 +8,21 @@
   $query = new WP_Query($args);
 
   while ( $query->have_posts() ) : $query->the_post(); ?>
-    <section>
-      <a href="<?php the_permalink(); ?>">
-        <h1><?php the_title(); ?></h1>
-        <p><?php the_field( 'featured_excerpt'); ?></p>
-        <?php the_post_thumbnail(); ?>
-      </a>
+    <section class="container-wide">
+      <article class="flex">
+        <div class="container-text bg-colour-main flex-item feature">
+          <h3><i class="fas fa-star fa-sm"></i> Featured cat</h3>
+          <a href="<?php the_permalink(); ?>">
+            <h1><?php the_title(); ?></h1>
+          </a>
+          <p><?php the_field( 'cat_excerpt'); ?></p>
+          <p></p>
+          <a href="<?php the_permalink(); ?>" class="btn btn-primary inverted">
+          Check out <?php the_title(); ?>
+          </a>
+        </div>
+          <?php the_post_thumbnail( 'large', ['class' => 'flex-item'] ); ?>
+      </article>
     </section>
   <?php endwhile;
   wp_reset_query();
