21b5d210e2b13b2f67a4a4ec6475cd2be7958ff5
krisztin <k.nagy@wellcome.ac.uk>
2018-07-12 15:51:26 +0100

    sidebar naming fix

--- sidebar.php
+++ sidebar.php
@@ -7,11 +7,11 @@
  * @package charity_shelter
  */
 
-if ( ! is_active_sidebar( 'sidebar' ) ) {
+if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	return;
 }
 ?>
 
 <aside id="secondary" class="widget-area">
-	<?php dynamic_sidebar( 'sidebar' ); ?>
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </aside><!-- #secondary -->
