bcc4c0205a63e723164048159e5431b278c0a283
krisztin <k.nagy@wellcome.ac.uk>
2018-07-12 10:54:23 +0100

    github link in footer along with personalisation

--- footer.php
+++ footer.php
@@ -13,9 +13,10 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer">
+	<footer id="colophon" class="site-footer container">
 		<div class="site-info container-full">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'charity_shelter' ) ); ?>">
+			<h3>Made by Kriszti | <a href="https://github.com/krisztin/wp-shelter-theme" target="_blank">Take a look behind the scenes on Github</a></h3>
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'charity_shelter' ) ); ?>"  target="_blank">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
 				printf( esc_html__( 'Proudly powered by %s', 'charity_shelter' ), 'WordPress' );
@@ -24,7 +25,7 @@
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'charity_shelter' ), 'charity_shelter', '<a href="http://underscores.me/">Underscores.me</a>' );
+				printf( esc_html__( 'Built on %1$s.' ), '<a href="http://underscores.me/" target="_blank">Underscores.me</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
